5f2da01bc2214172c68170bddfcfa09a52f383ec
Dakota <dakotacbrown@gmail.com>
2022-05-23 14:30:23 -0500

    Update AutomobileReport.py

--- Projects/Hadoop Mini Project/AutomobileReport.py
+++ Projects/Hadoop Mini Project/AutomobileReport.py
@@ -2,18 +2,20 @@ from mrjob.job import MRJob
 from mrjob.step import MRStep
 
 class AutoBreakdown(MRJob):
-    def steps(self):
-        return [
-            MRStep(mapper=self.mapper_get_vins,
-                   reducer=self.reducer_find_make_year)
-        ]
-
-    def mapper_get_vins(self, _, line):
-        (incident_id, incident_type, vin_number, make, model, year, incident_date, description) = line.split(',')
-        yield (vin_number, [incident_type, make, year])
-
-    def reducer_find_mak_year(self, key, values):
-        yield (key, [values.make, values.year]) 
+    def mapper(self, _, line):
+        incident_id, incident_type, vin_number, make, model, year, incident_date, description = line.split(',')
+        yield vin_number, [incident_type, make, year]
+    
+    def reducer(self, key, values):
+        for v in values:
+            if v[0] == "I":
+                make = v[1]
+                year = v[2]
+        for v in values:
+            if v[0] == "A":
+                v[1] = make
+                v[2] = year
+                yield key, v
 
 if __name__ == '__main__':
     AutoBreakdown.run()
