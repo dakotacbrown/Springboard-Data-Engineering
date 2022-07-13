e5728817ca8e50482d7ff37c571ab3e701ad2b8b
Dakota <dakotacbrown@gmail.com>
2022-07-13 15:44:44 -0500

    Optimizing Spark

--- Projects/20.16 Spark Optimization/optimize.py
+++ Projects/20.16 Spark Optimization/optimize.py
@@ -1,45 +1,30 @@
-'''
-Optimize the query plan
-
-Suppose we want to compose query in which we get for each question also the number of answers to this question for each month. See the query below which does that in a suboptimal way and try to rewrite it to achieve a more optimal plan.
-'''
-
-
 import pyspark
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, count, month
-
+#import broadcast
+from pyspark.sql.functions import count, month, broadcast
 import os
 
 
 spark = SparkSession.builder.appName('Optimize I').getOrCreate()
 
+#partition management
+spark.conf.set("spark.sql.shuffle.partitions", 4)
+
 base_path = os.getcwd()
 
-project_path = ('/').join(base_path.split('/')[0:-3]) 
+#wrong path used updated to correct path
+answers_input_path = 'data/answers'
 
-answers_input_path = os.path.join(project_path, 'data/answers')
+questions_input_path = 'data/questions'
 
-questions_input_path = os.path.join(project_path, 'output/questions-transformed')
+#cache the data frames in case they're used multiple times
+answersDF = spark.read.option('path', answers_input_path).load().cache()
 
-answersDF = spark.read.option('path', answers_input_path).load()
+questionsDF = spark.read.option('path', questions_input_path).load().cache()
 
-questionsDF = spark.read.option('path', questions_input_path).load()
+answers_month = answersDF.withColumn('month', month('creation_date')).groupBy('question_id', 'month').agg(count('*').alias('cnt')).cache()
 
-'''
-Answers aggregation
-
-Here we : get number of answers per question per month
-'''
-
-answers_month = answersDF.withColumn('month', month('creation_date')).groupBy('question_id', 'month').agg(count('*').alias('cnt'))
-
-resultDF = questionsDF.join(answers_month, 'question_id').select('question_id', 'creation_date', 'title', 'month', 'cnt')
+#broadcast the smaller DF to the bigger one
+resultDF = questionsDF.join(broadcast(answers_month), 'question_id').select('question_id', 'creation_date', 'title', 'month', 'cnt')
 
 resultDF.orderBy('question_id', 'month').show()
-
-'''
-Task:
-
-see the query plan of the previous result and rewrite the query to optimize it
-'''
